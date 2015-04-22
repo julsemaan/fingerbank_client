37b09ffe26d30b87af1cee8fe94f472c8f361cea
Julien Semaan <jul.semaan@gmail.com>
2015-04-21 20:07:31 -0400

    Revert "removed test dummy files"
    This reverts commit 48996db06e1a7b668f84bfa9e789e3dc69939fcd.

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '5a2e2b3a418430fb8cdb6e774c6ddbb373859ef1e6f43266fe2cf10c496d38cc5cb2acaac897060100d6477e7013c78ae86fe377078e7f80e5278f03bf2cbe8f'
